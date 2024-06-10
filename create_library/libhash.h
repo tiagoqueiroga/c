a753a8e8f2bc0f35be0ef959252f2da70825d8a3
tiago queiroga <tiagoqueiroga@gmail.com>
2024-06-09 23:38:53 -0300

    add a bunch of stuff

--- create_library/libhash.h
+++
@@ -1,6 +0,0 @@
-#ifndef _HASH_H
-#define _HASH_H
-
-unsigned long hash(unsigned char *str);
-
-#endif
--- raylib-first/game.c
+++ raylib-first/game.c
@@ -1,49 +1,121 @@
 #include <raylib.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
 
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 450
+#define GAME_TITLE "Raylib game"
 
+typedef struct
+{
+  float posX;
+  float posY;
+  float width;
+  float height;
+  float velocity;
+  Color color;
+} player;
 
-int main() {
+typedef struct
+{
+  float posX;
+  float posY;
+  float width;
+  float height;
+  float velocity;
+  Color color;
+} enemy;
 
-  float posX = 0;
-  float posY = 0;
+int generateRandomPos(int max)
+{
+  int pos;
+  srand(time(0));
+  pos = rand() % max + 1;
+  return pos;
+}
 
-  float velocity = 100;
+void drawPlayer(player *p)
+{
+  DrawRectangle(p->posX, p->posY, p->width, p->height, p->color);
+}
+
+void drawEnemy(enemy *e)
+{
+  DrawCircle(e->posX, e->posY, 20, e->color);
+}
+
+void printPlayerCords(player *p)
+{
+  printf("posX:(%f) posY:(%f)\n", p->posX, p->posY);
+}
+
+void handlePlayerControl(player *p)
+{
+
+  if (IsKeyDown(KEY_RIGHT) || IsKeyDown(KEY_L))
+  {
+
+    if (p->posX < (SCREEN_WIDTH - p->width))
+      p->posX += GetFrameTime() + p->velocity;
+  }
+
+  if (IsKeyDown(KEY_LEFT) || IsKeyDown(KEY_H))
+  {
+    if (p->posX > 0)
+      p->posX += GetFrameTime() - p->velocity;
+  }
+
+  if (IsKeyDown(KEY_UP) || IsKeyDown(KEY_K))
+  {
+    if (p->posY > 0)
+      p->posY += GetFrameTime() - p->velocity;
+  }
+
+  if (IsKeyDown(KEY_DOWN) || IsKeyDown(KEY_J))
+  {
+    if (p->posY < (SCREEN_HEIGHT - p->height))
+      p->posY += GetFrameTime() + p->velocity;
+  }
+}
+
+int main()
+{
+  player p1 = {
+      .posX = generateRandomPos(SCREEN_WIDTH),
+      .posY = generateRandomPos(SCREEN_HEIGHT),
+      .width = 50,
+      .height = 50,
+      .velocity = 5,
+      .color = BLUE};
+
+  enemy e = {
+      .posX = generateRandomPos(SCREEN_WIDTH),
+      .posY = generateRandomPos(SCREEN_HEIGHT),
+      .height = 20,
+      .width = 20,
+      .color = RED};
 
   SetTargetFPS(60);
 
-  InitWindow(800, 450, "Title");
+  InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, GAME_TITLE);
 
   while (!WindowShouldClose())
   {
 
-      if(IsKeyPressed(KEY_RIGHT))
-      {
-        posX += GetFrameTime() + velocity;
-      }
+    BeginDrawing();
 
-      if(IsKeyPressed(KEY_LEFT))
-      {
-        posX += GetFrameTime() - velocity;
-      }
+    printPlayerCords(&p1);
 
-      if(IsKeyPressed(KEY_UP))
-      {
-        posY += GetFrameTime() - velocity;
-      }      
+    ClearBackground(RAYWHITE);
 
-      if(IsKeyPressed(KEY_DOWN))
-      {
-        posY += GetFrameTime() + velocity;
-      }      
+    handlePlayerControl(&p1);
 
+    drawPlayer(&p1);
 
-      BeginDrawing();
-      
-      ClearBackground(RAYWHITE);
+    drawEnemy(&e);
 
-        DrawRectangle(posX,posY,100,100,BLUE);
-      
-      EndDrawing();
+    EndDrawing();
   }
 
   CloseWindow();
